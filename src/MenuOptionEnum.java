e78e7889cded0a12cf0ee8865e11502379be3b4b
Danyon <Danyongreymoon@gmail.com>
2016-10-14 11:58:48 -0600

    Refactored Program's menu method by replacing type fields with strategy

--- src/MenuOptionEnum.java
+++ src/MenuOptionEnum.java
@@ -1,9 +1,26 @@
 /**
  * Created by Greymoon on 10/6/2016.
  */
-public enum MenuOptionEnum {
-    EXIT,
-    PLAYER_VS_PLAYER,
-    PLAYER_VS_COMPUTER,
-    COMPUTER_VS_COMPUTER
+
+public enum MenuOptionEnum implements IMakeGameMethod{
+    EXIT{
+        public Game makeGameWithView(IView view){
+            return new Game();
+        }
+    },
+    PLAYER_VS_PLAYER{
+        public Game makeGameWithView(IView view){
+            return new Game(new Human(view), new Human(view));
+        }
+    },
+    PLAYER_VS_COMPUTER{
+        public Game makeGameWithView(IView view){
+            return new Game(new Human(view), new Computer(view));
+        }
+    },
+    COMPUTER_VS_COMPUTER{
+        public Game makeGameWithView(IView view){
+            return new Game(new Computer(view), new Computer(view));
+        }
+    }
 }
